505d11c0daefe0cc336764202bf1b8988a31825f
Rudraksh Tyagi <59254790+dragon-slayer875@users.noreply.github.com>
2023-12-28 17:56:08 +0530

    Turn buttons into components

--- src/timer.jsx
+++ src/timer.jsx
@@ -1,16 +1,36 @@
 import { useState } from "react";
 import { useEffect } from "react";
 
-export default function Timer() {
+function StartButton({ time, startTimer, resetTimer }) {
+    if (time === 0) {
+        return <button onClick={startTimer}>Start</button>;
+    }
+    else {
+        return <button onClick={resetTimer}>Reset</button>
+    }
+}
+
+function PauseButton({ time, pauseTimer }) {
+    if (time === 0) {
+        return <button disabled>Pause</button>;
+    }
+    else {
+        return <button onClick={pauseTimer}>Pause</button>;
+    }
+}
+
+function Timer() {
     const [time, setTime] = useState(0);
 
     return (
         <div className="main-body">
             <div className="buttons">
-                <button>Start</button>
-                <button>Pause</button>
+                <StartButton time={time} startTimer={() => {}} resetTimer={() => {}} />
+                <PauseButton time={time} pauseTimer={() => {}} />
             </div>
             <div className="timer">{time}</div>
         </div>
     );
 }
+
+export default Timer;
