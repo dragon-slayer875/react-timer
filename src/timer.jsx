7d95535e98eb891083eb77ad2d6220d59536674c
Rudraksh Tyagi <59254790+dragon-slayer875@users.noreply.github.com>
2023-12-28 18:03:48 +0530

    Add functions to manipulate timer

--- src/timer.jsx
+++ src/timer.jsx
@@ -4,17 +4,15 @@ import { useEffect } from "react";
 function StartButton({ time, startTimer, resetTimer }) {
     if (time === 0) {
         return <button onClick={startTimer}>Start</button>;
-    }
-    else {
-        return <button onClick={resetTimer}>Reset</button>
+    } else {
+        return <button onClick={resetTimer}>Reset</button>;
     }
 }
 
 function PauseButton({ time, pauseTimer }) {
     if (time === 0) {
         return <button disabled>Pause</button>;
-    }
-    else {
+    } else {
         return <button onClick={pauseTimer}>Pause</button>;
     }
 }
@@ -22,11 +20,31 @@ function PauseButton({ time, pauseTimer }) {
 function Timer() {
     const [time, setTime] = useState(0);
 
+    function startTimer() {
+        setInterval(() => {
+            setTime((time) => time + 1);
+        }, 1000);
+    }
+
+    function resetTimer() {
+        setTime(0);
+        clearInterval();
+    }
+
+    function pauseTimer() {
+        setTime(time);
+        clearInterval();
+    }
+
     return (
         <div className="main-body">
             <div className="buttons">
-                <StartButton time={time} startTimer={() => {}} resetTimer={() => {}} />
-                <PauseButton time={time} pauseTimer={() => {}} />
+                <StartButton
+                    time={time}
+                    startTimer={startTimer}
+                    resetTimer={ resetTimer}
+                />
+                <PauseButton time={time} pauseTimer={ pauseTimer } />
             </div>
             <div className="timer">{time}</div>
         </div>
